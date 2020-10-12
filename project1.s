903a15eed05a54c2b8b615104ccfdebdb7907910
Bipul Poudyal <bipul@Bipuls-MacBook-Pro.local>
2020-10-11 23:41:57 -0400

    User Input

--- project1.s
+++ project1.s
@@ -1,1 +1,10 @@
-Anmol_gautam
+#Bipul Poudyal
+#@02840816
+
+#My I.D number is 02840816. Since 02840816%11 =0; The base number is going to be 26.
+.data
+	input_message: .asciiz "Input a string: "
+  newline: .asciiz "\n"
+	Input_user: .space  10
+
+.text
