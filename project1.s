d60e601805360b2d8c396351d51197f953b851d6
Bipul Poudyal <bipul@Bipuls-MacBook-Pro.local>
2020-10-12 00:46:42 -0400

    Initial comments

--- project1.s
+++ project1.s
@@ -1,10 +1,5 @@
-#Bipul Poudyal
-#@02840816
+#This is the MIPS project 1 for Computer Organization I.
+#This program reads a string of exactly 10 characters (including spaces, excluding the null character at the end) from #user input and returns a number
 
-#My I.D number is 02840816. Since 02840816%11 =0; The base number is going to be 26.
-.data
-	input_message: .asciiz "Input a string: "
-  newline: .asciiz "\n"
-	Input_user: .space  10
-
-.text
+##Author: Bipul Poudyal
+##ID: @02840816   
