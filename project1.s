0d2908a49aaa4db0e5065a74330492e3160b5f2e
Bipul Poudyal <bipul@Bipuls-MacBook-Pro.local>
2020-10-12 01:00:26 -0400

    User Input

--- project1.s
+++ project1.s
@@ -2,4 +2,13 @@
 #This program reads a string of exactly 10 characters (including spaces, excluding the null character at the end) from #user input and returns a number
 
 ##Author: Bipul Poudyal
-##ID: @02840816   
+##ID: @02840816  
+
+#My I.D number is 02840816. Since 02840816%11 =0; The base number is going to be 26.
+
+ .data
+ 	input_message: .asciiz "Input a string: "
+   newline: .asciiz "\n"
+ 	Input_user: .space  10
+
+ .text 
